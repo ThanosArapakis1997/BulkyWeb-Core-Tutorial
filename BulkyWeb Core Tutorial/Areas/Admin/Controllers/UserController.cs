f179d9ef6762e1bfef24835cbd70ba82b7c88692
MikeZe <michaelzeakis13@gmail.com>
2024-04-17 22:13:30 +0300

    testare to re 8ano (name warning)

--- BulkyWeb Core Tutorial/Areas/Admin/Controllers/UserController.cs
+++ BulkyWeb Core Tutorial/Areas/Admin/Controllers/UserController.cs
@@ -6,6 +6,9 @@ using MGTConcerts.Utilities;
 using Microsoft.AspNetCore.Authorization;
 using MGTConcerts.Data;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Identity;
+
 
 namespace MGTConcerts.Areas.Admin.Controllers
 {
@@ -14,13 +17,16 @@ namespace MGTConcerts.Areas.Admin.Controllers
 
     public class UserController : Controller
     {
+        private readonly UserManager<IdentityUser> _userManager;
+
         //private readonly ApplicationDbContext _db;
         private readonly IUnitOfWork unitOfWork;
 
 
 
-        public UserController(IUnitOfWork _unitOfWork)
+        public UserController(IUnitOfWork _unitOfWork, UserManager<IdentityUser> userManager)
         {
+            _userManager = userManager;
             unitOfWork = _unitOfWork;
 
         }
@@ -39,8 +45,14 @@ namespace MGTConcerts.Areas.Admin.Controllers
         [HttpGet]
         public IActionResult GetAll()
         {
-           // List<ApplicationUser> objUserList = _db.ApplicationUsers.Include(u=>u.UserName).ToList();
-            List<ApplicationUser> objUserList = unitOfWork.ApplicationUser.GetAll().ToList();   
+            // List<ApplicationUser> objUserList = _db.ApplicationUsers.Include(u=>u.UserName).ToList();
+            // List<ApplicationUser> objUserList = unitOfWork.ApplicationUser.GetAll().ToList();   
+            //List<ApplicationUser> objUserList = User.FindAll(ClaimTypes.Name);
+            //var users = _userManager.Users;
+
+            List<IdentityUser> objUserList = _userManager.Users.ToList();
+
+
             return Json(new { data = objUserList });
         }
 
